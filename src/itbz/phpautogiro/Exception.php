17d5141dd4508714d675ef9875aed65bcddc0590
itbz <hannes.forsgard@gmail.com>
2012-10-08 15:03:03 +0200

    Added base exception

---
+++ src/itbz/phpautogiro/Exception.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is part of the STB package
+ *
+ * Copyright (c) 2011-12 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\phpautogiro
+ */
+
+namespace itbz\phpautogiro;
+
+/**
+ * Base exception
+ *
+ * @package itbz\phpautogiro
+ */
+class Exception extends \Exception
+{
+}
