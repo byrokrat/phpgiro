0f14f7f5c54c7df507257da558508e57aec1bfe4
itbz <hannes.forsgard@gmail.com>
2012-10-10 17:55:25 +0200

    Created parser

---
+++ src/itbz/phpautogiro/Parser.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is part of the STB package
+ *
+ * Copyright (c) 2012 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\phpautogiro
+ */
+
+namespace itbz\phpautogiro;
+
+use \DOMDocument;
+
+/**
+ * Parse AG files using designated strategy
+ *
+ * @package itbz\phpautogiro
+ */
+class Parser
+{
+    /**
+     * Parsing strategy
+     *
+     * @var ParserStrategyInterface
+     */
+    private $strategy;
+
+    /**
+     * Parse AG files using designated strategy
+     *
+     * @param ParserStrategyInterface $strategy
+     */
+    public function __construct(ParserStrategyInterface $strategy)
+    {
+        $this->strategy = $strategy;
+    }
+
+    /**
+     * Parse AG files using designated strategy
+     * 
+     * @param array $lines AG file contents
+     * 
+     * @return DOMDocument
+     */
+    public function parse(array $lines)
+    {
+        $this->strategy->clear();
+        foreach ($lines as $line) {
+            $this->strategy->parse($line);
+        }
+
+        $domDocument = $this->strategy->getDomDocument();
+
+        // Validate against DTD
+
+        return $domDocument;
+    }
+}
---
+++ src/itbz/phpautogiro/ParserFactory.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of the STB package
+ *
+ * Copyright (c) 2012 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\phpautogiro
+ */
+
+namespace itbz\phpautogiro;
+
+use Exception\StrategyException;
+use ParserStrategy\LayoutH;
+
+/**
+ * Create parsers for different AG file types
+ *
+ * @package itbz\phpautogiro
+ */
+class ParserFactory implements LayoutInterface
+{
+    /**
+     * Build a parser for AG file type denoted by flag
+     *
+     * @param int $flag One of the LayoutInterface flags
+     *
+     * @return Parser
+     *
+     * @throws StrategyException if flag is unknown
+     */
+    public function build($flag)
+    {
+        assert('is_int($flag)');
+
+        if ($flag == self::LAYOUT_H) {
+            $strategy = new LayoutH;
+        } else {
+            $msg = _('Unable to create parsing strategy: layout unknown.');
+            throw new StrategyException($msg);
+        }
+
+        return new Parser($strategy);
+    }
+}
