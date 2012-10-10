45e796c27362f53988de0034dc9f181cdcfb71b6
itbz <hannes.forsgard@gmail.com>
2012-10-10 17:53:32 +0200

    Added StrategyException

---
+++ src/itbz/phpautogiro/Exception/StrategyException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the STB package
+ *
+ * Copyright (c) 2012 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\phpautogiro\Exception
+ */
+
+namespace itbz\phpautogiro\Exception;
+
+use itbz\phpautogiro\Exception;
+
+/**
+ * Exception thrown when parsing strategy could not be created
+ *
+ * @package itbz\phpautogiro\Exception
+ */
+class StrategyException extends Exception
+{
+}
