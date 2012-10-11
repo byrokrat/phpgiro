6cfeab983d69c97fdf10ac67e4d0aea9fc37f81a
itbz <hannes.forsgard@gmail.com>
2012-10-11 13:23:38 +0200

    Added AgXmlConverterTest

---
+++ tests/unit/AgXmlConverterTest.php
@@ -0,0 +1,77 @@
+<?php
+namespace itbz\phpautogiro;
+
+class AgXmlConverterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConvert()
+    {
+        $sniffer = $this->getMock('itbz\phpautogiro\Sniffer');
+
+        $sniffer->expects($this->once())
+            ->method('sniff')
+            ->will($this->returnValue(LayoutInterface::LAYOUT_H));
+
+        $parserFactory = $this->getMock('itbz\phpautogiro\ParserFactory');
+
+        $parser = $this->getMock(
+            'itbz\phpautogiro\Parser\Parser',
+            array(),
+            array(),
+            '',
+            false
+        );
+
+        // Parser factory should build a parser using the sniffed layout id
+        $parserFactory->expects($this->once())
+            ->method('build')
+            ->with(LayoutInterface::LAYOUT_H)
+            ->will($this->returnValue($parser));
+
+        // Parser should be called
+        $parser->expects($this->once())
+            ->method('parse');
+
+        $converter = new AgXmlConverter($sniffer, $parserFactory);
+        $converter->convert(array());
+    }
+
+    public function testRemoveBlankLines()
+    {
+        $rawData = array(
+            'a' => "",
+            'b' => "  \r\n",
+            'c' => "foo",
+            'd' => " ",
+            'e' => "bar",
+            'f' => "\t\n "
+        );
+
+        $trimedData = array(
+            'c' => "foo",
+            'e' => "bar",
+        );
+
+        $sniffer = $this->getMock('itbz\phpautogiro\Sniffer');
+        $parserFactory = $this->getMock('itbz\phpautogiro\ParserFactory');
+
+        $parser = $this->getMock(
+            'itbz\phpautogiro\Parser\Parser',
+            array(),
+            array(),
+            '',
+            false
+        );
+
+        $parserFactory->expects($this->once())
+            ->method('build')
+            ->will($this->returnValue($parser));
+
+        // When parser recievd the data it should be trimmed
+        $parser->expects($this->once())
+            ->method('parse')
+            ->with($trimedData);
+
+        $converter = new AgXmlConverter($sniffer, $parserFactory);
+        $converter->convert($rawData);
+    }
+}
