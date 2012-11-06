af61cac85bc191f6b389453fb2db772c515695f8
itbz <hannes.forsgard@gmail.com>
2012-11-06 16:57:22 +0100

    Added ContentException

---
+++ src/itbz/swegiro/Exception/ContentException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of the swegiro package
+ *
+ * Copyright (c) 2012 Hannes Forsgård
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Hannes Forsgård <hannes.forsgard@gmail.com>
+ * @package itbz\swegiro\Exception
+ */
+
+namespace itbz\swegiro\Exception;
+
+use itbz\swegiro\Exception;
+
+/**
+ * Exception thrown when loaded content does not match a predefined rule
+ *
+ * @package itbz\swegiro\Exception
+ */
+class ContentException extends Exception
+{
+}
