157e4af0e6a9bb768615806740b22737b23ae81f
itbz <hannes.forsgard@gmail.com>
2013-01-17 17:55:35 +0100

    Updated copyright notice to 2013

--- src/itbz/swegiro/SnifferInterface.php
+++ src/itbz/swegiro/SnifferInterface.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the swegiro package
  *
- * Copyright (c) 2012 Hannes Forsgård
+ * Copyright (c) 2012-13 Hannes Forsgård
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
